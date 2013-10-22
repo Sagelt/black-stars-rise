c0e1eb9524befc2064454fe3489be2802d2be950
Sage LaTorra <sage@latorra.org>
2013-10-22 13:39:18 -0700

    Split careers and setups into individual files
    This allows settings to reuse components. It also breaks InDesign layout (for now) but hey, nobody else uses that yet, right?

--- configuration/site.py
+++ configuration/site.py
@@ -6,6 +6,14 @@ default_part_key = 'introduction'
 parts = Book([
   Chapter(key='introduction', title='Introduction', file_name='text/introduction.xml'),
   Setting(key='smalltown', title='Small Town', file_names=[
-                                    'text/careers/basic.xml',
+                                    'text/careers/doctor.xml',
+                                    'text/careers/reporter.xml',
+                                    'text/careers/librarian.xml',
+                                    'text/careers/detective.xml',
+                                    'text/careers/professor.xml',
+                                    'text/careers/mechanic.xml',
+                                    'text/careers/priest.xml',
+                                    'text/careers/artist.xml',
+                                    'text/setups/the-mansion-on-the-hill.xml',
   ])
 ])
