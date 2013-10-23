824d49b7e646e4e3655c2c01721b7861669107c1
Sage LaTorra <sage@latorra.org>
2013-10-22 18:59:51 -0700

    Add the rest of the basic moves

--- configuration/setup.py
+++ configuration/setup.py
@@ -4,6 +4,9 @@ black_stars_rise = Book([
   Chapter(key='introduction', title='Introduction', file_name='text/introduction.xml'),
   BasicMoveList(key='basicmoves', title='Basic Moves', file_names=[
                                     'text/basic_moves/take-a-risk.xml',
+                                    'text/basic_moves/suffer-harm.xml',
+                                    'text/basic_moves/keep-it-together.xml',
+                                    'text/basic_moves/study.xml',
   ]),
   Setting(key='smalltown', title='Small Town', file_names=[
                                     'text/careers/doctor.xml',
